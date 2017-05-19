47ef8624d2dba7bd2a671e30d8a81d5669c897d1
Victor Blomberg <blomberg14@gmail.com>
2017-05-19 11:56:22 +0700

    right email

--- app/mailers/purchase_mailer.rb
+++ app/mailers/purchase_mailer.rb
@@ -1,7 +1,7 @@
 class PurchaseMailer < ActionMailer::Base
 	layout 'purchase_mailer'
-	default from: 'Stripe payments, email: <victor@smartproduktion.se>'
-
+	default from: 'Stripe payments <victor@smartproduktion.se>'
+  
   def	purchase_receipt purchase
   	@purchase = purchase
   	mail to: purchase.email, subject: "Thanks boi"
